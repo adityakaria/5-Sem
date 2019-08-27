13d799e52882774fc001e4afd99e3b1ddf9dcfd9
Aditya J Karia <adityakaria01@gmail.com>
2019-08-28 01:25:15 +0530

    before wta lab evaluation

--- wta/main/assets/js/script.js
+++ wta/main/assets/js/script.js
@@ -1,11 +1,11 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 // function openPage(pageName, elmnt, color) {
@@ -15,33 +15,38 @@ function myFunction() {
 //     for (i = 0; i < tabcontent.length; i++) {
 //       tabcontent[i].style.display = "none";
 //     }
-  
+
 //     // Remove the background color of all tablinks/buttons
 //     tablinks = document.getElementsByClassName("tablink");
 //     for (i = 0; i < tablinks.length; i++) {
 //       tablinks[i].style.backgroundColor = "";
 //     }
-  
+
 //     // Show the specific tab content
 //     document.getElementById(pageName).style.display = "block";
-  
+
 //     // Add the specific color to the button used to open the tab content
 //     elmnt.style.backgroundColor = color;
 //   }
-  
-  // Get the element with id="defaultOpen" and click on it
-  // document.getElementById("defaultOpen").click(); 
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click(); 
 
 function validateSignUpForm() {
   var user_name = document.forms["sign-up"]["Name"].value;
   var phone_no = document.forms["sign-up"]["Phone"].value;
   var email_id = document.forms["sign-up"]["email"].value;
   var user_password = document.forms["sign-up"]["password"].value;
-  if (user_name != "") {
-    alert("Welcome " + user_name +"\nYour Account is Successfully created");
+  var user_password_confirm = document.forms["sign-up"]["confirm_password"].value;
+  if (user_password !== user_password_confirm) {
+    alert("Passwords do not match. Try again!")
   }
-  window.open('', '_blank');
-} 
+  else if (user_name != "") {
+    alert("Welcome " + user_name + "\nYour Account is Successfully created");
+    window.open('', '_blank');
+  }
+
+}
 
 function validateLoginForm() {
   var user_name = document.forms["login"]["Name"].value;
@@ -49,7 +54,16 @@ function validateLoginForm() {
   var email_id = document.forms["login"]["email"].value;
   var user_password = document.forms["login"]["password"].value;
   if (user_name != "") {
-    alert("Welcome " + user_name +"\nYour Account is Successfully created");
+    alert("Welcome " + user_name + "\nYour Account is Successfully created");
   }
   window.open('', '_blank');
-} 
+}
+
+function validateEmailForm() {
+  var email_id = document.forms["collect-email"]["email"].value;
+  if (email_id != "") {
+    alert("Congratulations!\n" + email_id + " has been subscibed");
+  }
+}
+
+
