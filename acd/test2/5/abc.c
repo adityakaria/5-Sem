c3263f3c312f48c8bbd658f920908b70692add95
Aditya Karia <adityakaria01@gmail.com>
2019-09-05 15:51:33 +0530

    acd lab2 completed

---
+++ acd/test2/5/abc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//comment
+int main()
+{
+	int a, b, c;
+	char k, j, l, m, n;
+	float p;
+	//comment;
+	scanf("%d", &a);
+	b = 2;
+	c = 5;
+	a = b + c;
+	double y;
+	printf("abcdprintf");
+	return 0;
+	//comment
+}
