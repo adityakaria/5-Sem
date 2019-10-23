7a91686e889da4c01f41b3a0f677b798f0de1aa5
Aditya Karia <adityakaria01@gmail.com>
2019-10-23 13:40:26 +0530

    vector addition 1 & 2 CUDA

--- pc/lab5/2_vecAdd/vecAdd.cu
+++ pc/lab5/2_vecAdd/vecAdd.cu
@@ -3,13 +3,18 @@
 
 #include <stdio.h>
 
-global void vecAdd (int *a, int *b, int *c);
+__global__ void vecAdd (int *a, int *b, int *c);
 
 int main() {
 	int a[N], b[N], c[N];
 	int *dev_a, *dev_b, *dev_c;
 
-	// initialize a and b with real values (NOT SHOWN) size = N * sizeof(int);
+	// initialize a and b with real values (NOT SHOWN)
+	int size = N * sizeof(int);
+	for (int i = 0; i < 200; i++) {
+		a[i] = i;
+		b[i] = i/2;
+	}
 
 	cudaMalloc((void**)&dev_a, size);
 	cudaMalloc((void**)&dev_b, size);
@@ -20,15 +25,20 @@ int main() {
 
 	vecAdd<<<(int)ceil(N/T),T>>>(dev_a,dev_b,dev_c);
 
-	cudaMemcpy(c, dev_c, size,cudaMemcpyDeviceToHost); cudaFree(dev_a);
+	cudaMemcpy(c, dev_c, size,cudaMemcpyDeviceToHost);
 
+	for (int i = 0; i < 200; i++) {
+		printf("%d + %d = %d\n", a[i], b[i], c[i]);
+	}
+
+	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
 
 	exit (0);
 }
 
-global void vecAdd (int *a, int *b, int *c) {
+__global__ void vecAdd (int *a, int *b, int *c) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < N) {
 		c[i] = a[i] + b[i];
